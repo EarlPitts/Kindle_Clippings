1dfe5c817b305b04b028527f8ac44b1ac2df66c7
Earl <k2sy12@inf.elte.hu>
2019-04-10 13:25:45 +0200

    Added tagging and write to file functions

---
+++ Functions.md
@@ -0,0 +1,20 @@
+# Functions
+- Parse Clippings
+- Store Clippings
+- Edit Clippings
+- Delete Clippings
+- List Clippings
+
+## Parse 
+- Read Clipping.txt
+- for each clipping:
+  - ask for tag
+  - if tag is x -> skip it
+  - else store in file for that tag
+
+Categories:
+  - no categories
+  - anki
+  - quote
+  - task
+  - review
--- Parser.py
+++ Parser.py
@@ -1,7 +1,7 @@
 FILENAME = "My Clippings.txt"
 
 def parse():
-    with open(FILENAME) as file:
+    with open("Kindle_Clippings_Parser/"+FILENAME) as file:
         data = file.readlines()
 
         clippings = []
@@ -9,17 +9,17 @@ def parse():
 
         for line in data:
             if line == "==========\n":
-                clippings.append(clipping)
+                clippings.append(clipping.copy())
                 clipping.clear()
             else:
                 clipping.append(line)
 
         for clipping in clippings:
-            manage_clipping(clipping)
+            handle_clipping(clipping)
+            print("next")
 
-            
 
-def manage_clipping(clipping):
+def handle_clipping(clipping):
     Title = clipping[0]
     Metadata = clipping[1].split("|")
     Page = Metadata[0]
@@ -29,6 +29,27 @@ def manage_clipping(clipping):
     print("Page Number: " + Page)
     print("Date: " + Date)
     print("Text: " + Text)
+
+    tag = input("Add tag: ")
+    if tag == "x":
+        return
+    
+    with open(Title + "_" + tag, "a") as file:
+        file.write(Text + "\n") 
+    file.close()
+
+            
+
+# def manage_clipping(clipping):
+#     Title = clipping[0]
+#     Metadata = clipping[1].split("|")
+#     Page = Metadata[0]
+#     Date = Metadata[1]
+#     Text = clipping[3]
+#     print("Title: " + Title)
+#     print("Page Number: " + Page)
+#     print("Date: " + Date)
+#     print("Text: " + Text)
 
 
 parse()
